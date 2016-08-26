78a88a3f6f790067a51a7c01563d95d10a2dc48e
Yu-Tsung Tai <yuttai@indiana.edu>
2016-08-25 22:38:01 -0400

    yuttai@login2:~/Contextuality-and-Nonlocality-in-Discrete-Quantum-Theory/Programs> qsub Interval-Valued_Probability_Measures_Symbolic_Computation.ShapleyValue.pbs 1435487 yuttai@login2:~/Contextuality-and-Nonlocality-in-Discrete-Quantum-Theory/Programs> qsub Interval-Valued_Probability_Measures_Symbolic_Computation.findEmptyCore.pbs 1435488 yuttai@login2:~/Contextuality-and-Nonlocality-in-Discrete-Quantum-Theory/Programs> showstart 1435487 job 1435487 requires 16 procs for 10:00:00:00
    Estimated Rsv based start in             11:20:24:32 on Tue Sep  6 19:00:00
    Estimated Rsv based completion in        21:20:24:32 on Fri Sep 16 19:00:00
    
    Best Partition: bigred2
    
    yuttai@login2:~/Contextuality-and-Nonlocality-in-Discrete-Quantum-Theory/Programs> showstart 1435488
    job 1435488 requires 16 procs for 9:00:00:00
    
    Estimated Rsv based start in             11:20:24:23 on Tue Sep  6 19:00:00
    Estimated Rsv based completion in        20:20:24:23 on Thu Sep 15 19:00:00
    
    Best Partition: bigred2

---
+++ Programs/Interval-Valued_Probability_Measures_Symbolic_Computation.findEmptyCore.m
@@ -0,0 +1,14 @@
+(* ::Package:: *)
+
+(* ::Title:: *)
+(*Non-Additive Probability Measures Symbolic Computation*)
+
+
+NotebookEvaluate[NotebookOpen[FileNameJoin[{NotebookDirectory[],"Interval-Valued Probability Measures.nb"}]],InsertResults->False]
+
+
+(* ::Section:: *)
+(*Symbolic Computation*)
+
+
+Print[findIntervalValuedProbabilityMeasureWithEmptyCore[{a,b,c}]]
