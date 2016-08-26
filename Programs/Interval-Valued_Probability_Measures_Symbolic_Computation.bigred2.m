837e5efaf35ecefa62820e7abd63bd8c8729ff16
Yu-Tsung Tai <yuttai@indiana.edu>
2016-08-25 21:55:18 -0400

    yuttai@login2:~/Contextuality-and-Nonlocality-in-Discrete-Quantum-Theory/Programs> qsub Interval-Valued_Probability_Measures_Symbolic_Computation.bigred2.pbs 1435482 yuttai@login2:~/Contextuality-and-Nonlocality-in-Discrete-Quantum-Theory/Programs> showstart 1435482 job 1435482 requires 16 procs for 11:00:00:00
    Estimated Rsv based start in             11:21:13:35 on Tue Sep  6 19:00:00
    Estimated Rsv based completion in        22:21:13:35 on Sat Sep 17 19:00:00
    
    Best Partition: bigred2

---
+++ Programs/Interval-Valued_Probability_Measures_Symbolic_Computation.bigred2.m
@@ -0,0 +1,14 @@
+(* ::Package:: *)
+
+(* ::Title:: *)
+(*Non-Additive Probability Measures Symbolic Computation*)
+
+
+NotebookEvaluate[NotebookOpen[FileNameJoin[{NotebookDirectory[],"Interval-Valued Probability Measures.nb"}]],InsertResults->False]
+
+
+(* ::Section:: *)
+(*Symbolic Computation*)
+
+
+Print[checkEveryIntervalValuedProbabilityMeasureHasNonemptyCore[{a,b,c,d}]]
---
+++ Programs/Interval-Valued_Probability_Measures_Symbolic_Computation.bigred2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+module load mathematica
+cd "/N/u/yuttai/BigRed2/Contextuality-and-Nonlocality-in-Discrete-Quantum-Theory/Programs"
+math -script Interval-Valued_Probability_Measures_Symbolic_Computation.bigred2.m >> Interval-Valued_Probability_Measures_Symbolic_Computation.bigred2.txt
