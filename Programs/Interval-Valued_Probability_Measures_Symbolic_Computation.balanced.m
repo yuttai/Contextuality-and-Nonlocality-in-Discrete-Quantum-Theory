072b7bdb89894e9595ed790ae32cf0d115bcb79f
Yu-Tsung Tai <yuttai@indiana.edu>
2016-08-26 16:45:54 -0400

    yuttai@login2:~/Contextuality-and-Nonlocality-in-Discrete-Quantum-Theory/Programs> qsub Interval-Valued_Probability_Measures_Symbolic_Computation.balanced.pbs 1435780 yuttai@login2:~/Contextuality-and-Nonlocality-in-Discrete-Quantum-Theory/Programs> showstart 1435780 job 1435780 requires 16 procs for 8:00:00:00
    Estimated Rsv based start in              1:14:09:31 on Sun Aug 28 06:54:30
    Estimated Rsv based completion in         9:14:09:31 on Mon Sep  5 06:54:30
    
    Best Partition: bigred2

---
+++ Programs/Interval-Valued_Probability_Measures_Symbolic_Computation.balanced.m
@@ -0,0 +1,14 @@
+(* ::Package:: *)
+
+(* ::Title:: *)
+(*Non-Additive Probability Measures Examples*)
+
+
+NotebookEvaluate[NotebookOpen[FileNameJoin[{NotebookDirectory[],"Interval-Valued Probability Measures.nb"}]],InsertResults->False]
+
+
+(* ::Section:: *)
+(*Symbolic Computation*)
+
+
+Print[checkEveryIntervalValuedProbabilityMeasureBalanced[{a,b}]]
