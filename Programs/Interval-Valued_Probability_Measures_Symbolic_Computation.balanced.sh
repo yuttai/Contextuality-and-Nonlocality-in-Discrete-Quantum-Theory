e22371c699dbe4467680ff1bc7ce9b30ccbfb741
Yu-Tsung Tai <yuttai@indiana.edu>
2016-08-28 22:56:27 -0400

    Clean up files in bigred2.uits.iu.edu without results...

--- Programs/Interval-Valued_Probability_Measures_Symbolic_Computation.balanced.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-module load mathematica
-cd "/N/u/yuttai/BigRed2/Contextuality-and-Nonlocality-in-Discrete-Quantum-Theory/Programs"
-math -script Interval-Valued_Probability_Measures_Symbolic_Computation.balanced.m >> Interval-Valued_Probability_Measures_Symbolic_Computation.balanced.txt
